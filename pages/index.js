5993df96554c9b657765eae5458d22b32661f2f8
siska199 <siska.apriana.1703226@students.um.ac.id>
2022-05-13 11:01:53 +0700

    create UI button login

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 const Home = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-
-    </div>
-  )
+  return <div className="flex justify-center h-screen w-screen items-center">
+    <button className="bg-blue-500 px-5 py-2 rounded-md text-white">Login With Google</button>
+  </div>
 }
 
 export default Home
